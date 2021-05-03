a7ae038b5dbc6c64477a74d4e16532e51d05bf2c
Joe <overtonje@gmail.com>
2021-05-03 15:25:49 -0500

    adding datamodels

--- levelup-csharp/Models/Game.cs
+++ levelup-csharp/Models/Game.cs
@@ -7,5 +7,10 @@ namespace levelup_csharp.Models
 {
     public class Game
     {
+        public int Id { get; set; }
+        public int GamerId { get; set; }
+        public int NumberOfPlayers { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
     }
 }
