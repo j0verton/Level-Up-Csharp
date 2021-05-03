c8ccad36aabdb0badbd763f15e2a67f988419388
Joe <overtonje@gmail.com>
2021-05-03 15:10:30 -0500

    adding datamodels

---
+++ levelup-csharp/Models/EventGamer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace levelup_csharp.Models
+{
+    public class EventGamer
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        public int GamerId { get; set; }
+    }
+}
